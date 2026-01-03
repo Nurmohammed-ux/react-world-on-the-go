d685dba87d7348fabdc744f3876d680852bb7414
Nurmohammed-ux <hridoynmh@gmail.com>
2026-01-03 22:44:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+
+  return (
+    <>
+      <h1>React World On the Go</h1>
+      <Countries/>
+    </>
+  )
+}
+
+export default App
